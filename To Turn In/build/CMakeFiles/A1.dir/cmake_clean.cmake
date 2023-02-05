55b21f2a4eddb8acf9c1069a5e887deef1aaa946
yida-zou <yida.zou@tamu.edu>
2023-02-05 15:37:40 -0600

    finished all tasks

---
+++ To Turn In/build/CMakeFiles/A1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "A1"
+  "A1.pdb"
+  "CMakeFiles/A1.dir/src/Image.cpp.o"
+  "CMakeFiles/A1.dir/src/Image.cpp.o.d"
+  "CMakeFiles/A1.dir/src/main.cpp.o"
+  "CMakeFiles/A1.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/A1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
